b63cbebf12418115225472932b9a668b267ea018
NicolasSilvestrini <nsilvestrini@itba.edu.ar>
2025-05-29 18:19:40 -0300

    lector de csv

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import csv
+def leer_csv(file):
+    with open(file,'r',encoding='utf-8') as archivo:
+        lector=csv.DictReader(archivo)
+        return list(lector)
+
+print(leer_csv('conexiones.csv'))
