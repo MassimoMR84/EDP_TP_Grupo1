f5468def151045fb5d34961e428ad2370097a02d
fpedrottiitba <fpedrotti@itba.edu.ar>
2025-06-05 16:49:55 -0300

    Comenté bien los pasos de cada cosa (antes estaba hecho con # y ahora con ''') y se agregaron validaciones que estaban en clases Nodo y Conexión a las validaciones generales

--- conexion.py
+++ conexion.py
@@ -2,62 +2,63 @@ from validaciones import *
 from nodo import *
 
 class Conexion:
-    
-    tiposDisponibles=("Ferroviaria", "Automotor", "Fluvial", "Aerea") #tupla porque no se pueden modificar
-    
+    '''Clase Conexión: contiene el nodo origen, el nodo destino, la distancia entre ellos, la restricción (si la hay) 
+    y el valor de la restricción (si corresponde)'''
+
     def __init__(self, origen: Nodo, destino: Nodo, tipo: str, distancia: int, restriccion=None, valorRestriccion=None):
         Conexion.validarNodo(origen)
         self.origen=origen
         Conexion.validarNodo(destino)
         self.destino=destino
-        Conexion.validarTipo(tipo)
+        validar_cadena(tipo)
+        validar_modo_de_transporte(tipo)
         self.tipo=tipo
         validar_numero_mayor_a_cero(distancia)
         self.distancia=distancia
         self.restriccion=restriccion
         self.valorRestriccion=valorRestriccion
 
-    def __repr__(self): #método __repr__ para modificar el comportamoiento de la función print()
+    def __repr__(self):
+        '''método __repr__ para modificar el comportamoiento de la función print()'''
         return f"Origen: {self.origen}\nDestino: {self.destino}\nTipo: {self.tipo}\nDistancia: {self.distancia}\nRestricción: {self.restriccion}\nValor restrictivo: {self.valorRestriccion}\n"
 
 
     @staticmethod
-    def validarNodo(nodo): #se valida que tanto el origen como el destino sean objetos de tipo Nodo
+    def validarNodo(nodo):
+        '''se valida que tanto el origen como el destino sean objetos de tipo Nodo'''
         if not isinstance(nodo, Nodo):
             raise TypeError("El origen y destino deben ser objetos de tipo Nodo")
         
-    @staticmethod
-    def validarTipo(tipo): #se valida que el tipo sea una cadena y que dicha cadena sea un tipo de transporte válido (uno de los que están presentes en la tupla inicial)
-        if not isinstance(tipo,str):
-            raise TypeError("El tipo debe ser una cadena")
-        if tipo not in Conexion.tiposDisponibles:
-            raise ValueError("El tipo de transporte debe ser uno de los siguientes: Ferroviaria, Automotor, Fluvial o Aerea")
         
-    @staticmethod
-    def validarDistancia(distancia: int):
-        if not isinstance(distancia, int): #se asume que la distancia siempre será un número sin decimales en km
-            raise TypeError("La distancia debe ser un número")
-        if distancia<=0:
-            raise ValueError("La distancia no puede ser menor o igual que cero")
-        
-def leer_conexiones(path, nodos: dict): #función para leer las conexiones del csv, se le pasan como parámetros el path del archivo y el diccionario de nodos obtenido con la función leer_nodos
-    with open(path, newline='', encoding='utf-8') as f: #se abre el csv de nodos, urf-8 para leer el archivo correctamente
-        reader = csv.DictReader(f) #se convierte cada fila en un diccionario
+def leer_conexiones(path, nodos: dict): 
+    '''función para leer las conexiones del csv, se le pasan como parámetros el path del archivo y el diccionario de nodos 
+    obtenido con la función leer_nodos'''
+    with open(path, newline='', encoding='utf-8') as f: 
+        '''se abre el csv de nodos, urf-8 para leer el archivo correctamente'''
+        reader = csv.DictReader(f) 
+        '''se convierte cada fila en un diccionario'''
         for row in reader:
-            origen = nodos.get(row['origen']) #se obtiene el nodo origen del diccionario de nodos
-            destino = nodos.get(row['destino']) #se obtiene el nodo destino del diccionrio de nodos
+            origen = nodos.get(row['origen']) 
+            '''se obtiene el nodo origen del diccionario de nodos'''
+            destino = nodos.get(row['destino']) 
+            '''se obtiene el nodo destino del diccionrio de nodos'''
             tipo = row['tipo']
-            distancia = int(row['distancia_km']) #se lo convierte a int porque todos los valores se miden en km y no tienen decimales
+            distancia = int(row['distancia_km']) 
+            '''se lo convierte a int porque todos los valores se miden en km y no tienen decimales'''
             restriccion = row.get('restriccion') or None
-            valor = row.get('valor_restriccion') or None #como puede haber restricción o no, valor y restricción pueden llegar a estar vacíos
+            valor = row.get('valor_restriccion') or None 
+            '''como puede haber restricción o no, valor y restricción pueden llegar a estar vacíos'''
 
-            if origen and destino: #si el origen y el destino realmente son nodos y existen
-                conexion = Conexion(origen, destino, tipo, distancia, restriccion, valor) #se instancia la conexión
-                origen.agregarConexiones(conexion)  #se agrega la conexión al nodo origen
+            if origen and destino: 
+                '''si el origen y el destino realmente son nodos y existen'''
+                conexion = Conexion(origen, destino, tipo, distancia, restriccion, valor)
+                '''se instancia la conexión'''
+                origen.agregarConexiones(conexion)
+                '''se agrega la conexión al nodo origen'''
 
 
 nodos=leer_nodos("/Users/federicopedrotti/Desktop/Estructuras de Datos y Programación/EDP_TP_Grupo1/nodos.csv")
 conexiones=leer_conexiones("/Users/federicopedrotti/Desktop/Estructuras de Datos y Programación/EDP_TP_Grupo1/conexiones.csv",nodos)
 
 print(nodos["Buenos_Aires"].conexiones)
-#Prueba funciona bien
+'''Prueba funciona bien'''
--- nodo.py
+++ nodo.py
@@ -1,30 +1,40 @@
 from validaciones import *
 
 class Nodo():
+    """Clase Nodo: contiene el nombre del nodo y sus conexiones """
     
     def __init__(self, nombre:str):
         validar_cadena(nombre)
         self.nombre=nombre
-        self.conexiones=[] #lista de conexiones; se elige una lista ya que se va a almacenar un conjunto de datos que pueden variar duarnte la ejecución
-                            # (se agregan conexiones cuando se lee el csv) y no se usa una pila o una cola porque no hace fakta utilizar lógicas como FIFO 
-                            # o LIFO; por eso la lista parece ser lo más adecuado
+        self.conexiones=[] 
+        '''lista de conexiones; se elige una lista ya que se va a almacenar un conjunto de datos que pueden variar duarnte la ejecución
+        (se agregan conexiones cuando se lee el csv) y no se usa una pila o una cola porque no hace fakta utilizar lógicas como FIFO o 
+        LIFO; por eso la lista parece ser lo más adecuado'''
 
     def agregarConexiones(self, conexion):
-        self.conexiones.append(conexion) #método de instancia para agregar una conexión a un nodo
+        '''método de instancia para agregar una conexión a un nodo'''
+        self.conexiones.append(conexion)
     
     def __repr__(self):
-        return f"{self.nombre}" #método __repr__ para modificar el comportamoiento de la función print()
+        '''método __repr__ para modificar el comportamoiento de la función print()'''
+        return f"{self.nombre}" 
         
 
 import csv
 
 def leer_nodos(path):
-    nodos = {} #diccionario para almacenar los datos: key: nombre del nodo--> value: objeto nodo con ese nombre
-    with open(path, newline='', encoding='utf-8') as f: #se abre el csv de nodos, urf-8 para leer el archivo correctamente
-        reader = csv.DictReader(f) #se convierte cada fila en un diccionario
-        for row in reader: #recorre las filas creadas
+    nodos = {} 
+    '''diccionario para almacenar los datos: key: nombre del nodo--> value: objeto nodo con ese nombre'''
+    with open(path, newline='', encoding='utf-8') as f: 
+        '''se abre el csv de nodos, urf-8 para leer el archivo correctamente'''
+        reader = csv.DictReader(f) 
+        '''se convierte cada fila en un diccionario'''
+        for row in reader:
+            '''recorre las filas creadas'''
             nombre = row['nombre']
             if nombre not in nodos:
-                nodos[nombre] = Nodo(nombre) #se crea un nodo si no estaba ya creado
-    return nodos #devuelve el diccionario con los nodos del csv
+                nodos[nombre] = Nodo(nombre) 
+                '''se crea un nodo si no estaba ya creado'''
+    return nodos 
+'''devuelve el diccionario con los nodos del csv'''
 
