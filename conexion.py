39b59091fd557aec763cf09f24f9d6977457478b
fpedrottiitba <fpedrotti@itba.edu.ar>
2025-06-13 16:55:36 -0300

    LAs lecturas de los csv de nodos y conexiones ahora se hacen con métodos estáticos dentro de la clase Nodo

--- conexion.py
+++ conexion.py
@@ -1,17 +1,12 @@
 from validaciones import *
-from nodo import *
-from SistemaTransporte import *
-import csv
 
 class Conexion:
     '''Clase Conexión: contiene el nodo origen, el nodo destino, la distancia entre ellos, la restricción (si la hay) 
     y el valor de la restricción (si corresponde)'''
 
     
-    def __init__(self, origen: Nodo, destino: Nodo, tipo: str, distancia: int, restriccion=None, valorRestriccion=None):
-        validarNodo(origen)
+    def __init__(self, origen, destino, tipo: str, distancia: int, restriccion=None, valorRestriccion=None):
         self.origen = origen
-        validarNodo(destino)
         self.destino = destino
         self.tipo = validar_modo_de_transporte(tipo)
         self.distancia = validar_numero_mayor_a_cero(distancia)
@@ -41,44 +36,3 @@ class Conexion:
 
         return atributo >= float(self.valorRestriccion)
         
-        
-def procesar_conexiones(path, nodos: dict): #! Juntar los lectores en un solo archivo y en una funcion
-    '''función para leer las conexiones del csv, instanciar los objetos conexión a partir de eso y agregarlos a los nodos correspondientes; se le pasan como parámetros el path del archivo y el diccionario de nodos 
-    obtenido con la función leer_nodos'''
-    with open(path, newline='', encoding='utf-8') as f: 
-        '''se abre el csv de nodos, urf-8 para leer el archivo correctamente'''
-        reader = csv.DictReader(f) 
-        '''se convierte cada fila en un diccionario'''
-        for row in reader:
-            origen = nodos.get(row['origen']) 
-            '''se obtiene el nodo origen del diccionario de nodos'''
-            destino = nodos.get(row['destino']) 
-            '''se obtiene el nodo destino del diccionrio de nodos'''
-            tipo = row['tipo']
-            distancia = int(row['distancia_km']) 
-            '''se lo convierte a int porque todos los valores se miden en km y no tienen decimales'''
-            restriccion = row.get('restriccion') or None
-            valor = row.get('valor_restriccion') or None 
-            '''como puede haber restricción o no, valor y restricción pueden llegar a estar vacíos'''
-
-            if origen and destino: 
-                '''si el origen y el destino realmente son nodos y existen'''
-                conexion = Conexion(origen, destino, tipo, distancia, restriccion, valor)
-                '''se instancia la conexión'''
-                if isinstance(conexion, Conexion):
-                    '''se chequea que la conexión realmente sea un objeto de dicha clase'''
-                    origen.agregarConexiones(conexion)
-                    '''se agrega la conexión al nodo origen'''
-                else:
-                    raise TypeError("Las conexiones no se pudieron agregar a los nodos correspondientes")
-
-
-
-'''Prueba de funcionamiento local'''
-if __name__ == "__main__":
-    nodos=leer_nodos("nodos.csv")
-    print(nodos)
-
-    procesar_conexiones("conexiones.csv", nodos)
-    print(nodos["Buenos_Aires"].conexiones)
-    '''Clases nodo y conexión funcionan OK'''
--- nodo.py
+++ nodo.py
@@ -1,4 +1,6 @@
 from validaciones import validar_cadena
+from conexion import Conexion
+import csv
 
 class Nodo():
 
@@ -23,23 +25,60 @@ class Nodo():
 
     def agregarConexiones(self, conexion):
         self.conexiones.append(conexion)
-        
 
-import csv
+    @staticmethod
+    def leer_nodos(path):   #! Juntar los lectores en un solo archivo y en una funcion
+        nodos = {} 
+        '''diccionario para almacenar los datos: key: nombre del nodo--> value: objeto nodo con ese nombre'''
+        with open(path, newline='', encoding='utf-8') as f: 
+            '''se abre el csv de nodos, urf-8 para leer el archivo correctamente'''
+            reader = csv.DictReader(f) 
+            '''se convierte cada fila en un diccionario'''
+            for row in reader:
+                '''recorre las filas creadas'''
+                nombre = row['nombre']
+                if nombre not in nodos:
+                    nodos[nombre] = Nodo(nombre) 
+                    '''se crea un nodo si no estaba ya creado'''
+        return nodos
+    
+    @staticmethod
+    def generar_conexiones(path, nodos: dict):
+        '''función para leer las conexiones del csv, instanciar los objetos conexión a partir de eso 
+        y agregarlos a los nodos correspondientes; se le pasan como parámetros el path del archivo 
+        y el diccionario de nodos obtenido con la función leer_nodos'''
 
-def leer_nodos(path):   #! Juntar los lectores en un solo archivo y en una funcion
-    nodos = {} 
-    '''diccionario para almacenar los datos: key: nombre del nodo--> value: objeto nodo con ese nombre'''
-    with open(path, newline='', encoding='utf-8') as f: 
-        '''se abre el csv de nodos, urf-8 para leer el archivo correctamente'''
-        reader = csv.DictReader(f) 
-        '''se convierte cada fila en un diccionario'''
-        for row in reader:
-            '''recorre las filas creadas'''
-            nombre = row['nombre']
-            if nombre not in nodos:
-                nodos[nombre] = Nodo(nombre) 
-                '''se crea un nodo si no estaba ya creado'''
-    return nodos 
-'''devuelve el diccionario con los nodos del csv'''
+        with open(path, newline='', encoding='utf-8') as f:
+            '''se abre el csv de nodos, utf-8 para leer el archivo correctamente'''
+            reader = csv.DictReader(f)
+            '''se convierte cada fila en un diccionario'''
+            for row in reader:
+                '''recorre las filas creadas'''
+                origen = row['origen'].strip()
+                '''se obtiene el nodo origen del diccionario de nodos'''
+                destino = row['destino'].strip()
+                '''se obtiene el nodo destino del diccionario de nodos'''
+                tipo = row['tipo']
+                distancia = float(row['distancia_km'])
+                '''se lo convierte a int porque todos los valores se miden en km y no tienen decimales'''
+                restriccion = row.get('restriccion') or None
+                valor = row.get('valor_restriccion') or 0 if restriccion else None
+                '''como puede haber restricción o no, valor y restricción pueden llegar a estar vacíos'''
 
+                if origen in nodos and destino in nodos:
+                    '''si el origen y el destino realmente son nodos y existen'''
+                    nodo_origen = nodos[origen]
+                    nodo_destino = nodos[destino]
+                    conexion = Conexion(nodo_origen, nodo_destino, tipo, distancia, restriccion, valor)
+                    nodo_origen.agregarConexiones(conexion)
+                    '''se agrega la conexión al nodo origen'''
+                else:
+                    raise TypeError(f"Las conexiones no se pudieron agregar a los nodos correspondientes. Nodo faltante: {origen} o {destino}")
+                
+
+'''Prueba de funcionamiento'''
+if __name__=="__main__":
+    nodos=Nodo.leer_nodos("nodos.csv")
+    print(nodos)
+    Nodo.generar_conexiones("conexiones.csv", nodos)
+    print(nodos['Buenos_Aires'].conexiones)
